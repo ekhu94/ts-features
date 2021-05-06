6c9594bf1b2ae76887b478ec05837db06eabe08e
Erik Huang <ekhu94@gmail.com>
2021-05-06 09:19:45 -0700

    uses destructuring in object input to get values

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -16,3 +16,35 @@ const multiple = (x: number, y: number) => {
 const divide = function (x: number, y: number): number {
   return x / y;
 };
+
+//! function with no return value takes void as return type
+const logger = (msg: string): void => {
+  console.log(msg);
+};
+
+//! in extremely rare case of throwing errors, return type is NEVER
+const error = (msg: string): never => {
+  throw new Error(msg);
+};
+
+//! pass in an object input, and use destructuring to provide the specific keys in object
+
+//? Object
+const todaysWeather: { date: Date; weather: string } = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+//? Weather function
+const declareWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(`Today's date is ${date}.`);
+  console.log(`Today's weather is ${weather}.`);
+};
+
+console.log(declareWeather(todaysWeather));
