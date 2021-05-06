04c46fad2c74f48bcd18c79486d30350f957d2b7
Erik Huang <ekhu94@gmail.com>
2021-05-06 15:15:52 -0700

    adds more complex types to interface and method

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,23 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
-const oldNissan = {
+const newNissan = {
   name: 'nissan',
-  year: 1999,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Vehicle name: ${this.name}`;
+  },
 };
 
-const printVehicle = ({ name, year, broken }: Vehicle): void => {
-  console.log(`Vehicle Name: ${name}`);
-  console.log(`Made In: ${year}`);
-  console.log(`Broken? ${broken}`);
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
+  console.log(`Made In: ${vehicle.year}`);
+  console.log(`Broken? ${vehicle.broken}`);
 };
 
-printVehicle(oldNissan);
+printVehicle(newNissan);
